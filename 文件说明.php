2ef6b11dc4b1f8f8d28616d27ac5f1aa884c715a
11223ren <1132974092@qq.com>
2019-05-14 14:56:34 +0800

    前台商户中心修改

--- 文件说明.php
+++ 文件说明.php
@@ -3,9 +3,11 @@
 
 html文件
 \template\m03stmhui201807yh07\homepage
+\template\m03stmhui201807yh07\member\home.htm修改公司网页设置的几个文字提示
 
 php数据处理
 \module\company
+\module\member\home.inc.php修改禁止前台用户修改公司网页设置
 
 总站HTML
 \template\m03stmhui201807yh07\foot.html 
