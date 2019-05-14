747c498470db46e52f16115d80b7c21d784a8150
11223ren <1132974092@qq.com>
2019-05-14 14:30:12 +0800

    主站首页修改

---
+++ 文件说明.php
@@ -0,0 +1,15 @@
+ HTMLcss等样式引入
+\company\htmlnew 
+
+html文件
+\template\m03stmhui201807yh07\homepage
+
+php数据处理
+\module\company
+
+总站HTML
+\template\m03stmhui201807yh07\foot.html 
+\template\m03stmhui201807yh07\header.html 
+
+\skin\m03sred\style\css\style.css
+
